b971851dc55beb9953b4e5bd1ff2ed5187ac290d
葛佩文 <nick950222>
2020-06-07 17:04:19 +0800

    [fix] little fix

--- src/pages/today/index.tsx
+++ src/pages/today/index.tsx
@@ -126,7 +126,7 @@ class Day extends Component<ITodayProblem, ITodayProblemState> {
    */
   async getProblemDetail(slug: string) {
     const problemDetail = await NetworkManager.getProblem(undefined, slug)
-    this.setState({ detail: problemDetail, problemString: problemDetail.translatedContent.replace(/\<\w+\>|\<\/\w+\>|\&\w+|/g, '').replace(/\s/g, '').replace(/\/|\\/, '') })
+    this.setState({ detail: problemDetail, problemString: problemDetail.translatedContent.replace(/\<\w+\>|\<\/\w+\>|\&\w+|/g, '').replace(/\s/g, '') })
   }
   /**
    * 获取统计信息
